69fe440cccae72f7448977df0bf53d0468ae2395
Davide De Marco <daveguitar.dema@gmail.com>
2020-08-18 18:07:34 +0200

    Upload file and Show Waveform

--- Grana/Source/PluginEditor.cpp
+++ Grana/Source/PluginEditor.cpp
@@ -10,31 +10,113 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-LaGranaAudioProcessorEditor::LaGranaAudioProcessorEditor (LaGranaAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
-{
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
+LaGranaAudioProcessorEditor::LaGranaAudioProcessorEditor (LaGranaAudioProcessor& p): AudioProcessorEditor (&p), audioProcessor (p){
+
+    formatManager.registerBasicFormats();
+    loadBtn = new juce::TextButton();
+    addAndMakeVisible(loadBtn);
+    loadBtn->setButtonText("Open...");
+    loadBtn->onClick = [this] {loadBtnClicked();};
+    loadBtn->setBounds(10, 10, 50, 25);
+
+
+
+    thumbnailCache = new juce::AudioThumbnailCache(5);
+    thumbnail = new  juce::AudioThumbnail(512, formatManager, *thumbnailCache);
+
+    thumbnail->addChangeListener(this);
+
+
     setSize (400, 300);
 }
 
 LaGranaAudioProcessorEditor::~LaGranaAudioProcessorEditor()
 {
+    delete loadBtn;
+    delete thumbnailCache, thumbnail;
 }
 
 //==============================================================================
 void LaGranaAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    juce::Rectangle<int> thumbnailBounds(40, 100, getWidth() - 20, getHeight() - 120);
+
+    if (thumbnail->getNumChannels() == 0)
+        paintIfNoFileLoaded(g, thumbnailBounds);
+    else
+        paintIfFileLoaded(g, thumbnailBounds);
 }
 
 void LaGranaAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+}
+
+
+void LaGranaAudioProcessorEditor::paintIfNoFileLoaded(juce::Graphics& g, const juce::Rectangle<int>& thumbnailBounds)
+{
+    
+    g.setColour(juce::Colours::darkgrey);
+    g.fillRect(thumbnailBounds);
+    g.setColour(juce::Colours::white);
+    g.drawFittedText("No File Loaded", thumbnailBounds, juce::Justification::centred, 1);
+    
+}
+
+void LaGranaAudioProcessorEditor::paintIfFileLoaded(juce::Graphics& g, const juce::Rectangle<int>& thumbnailBounds)
+{
+
+    
+    g.setColour(juce::Colours::white);
+    g.fillRect(thumbnailBounds);
+
+    g.setColour(juce::Colours::red);                               // [8]
+
+    thumbnail->drawChannels(g,                                      // [9]
+        thumbnailBounds,
+        0.0,                                    // start time
+        thumbnail->getTotalLength(),             // end time
+        1.0f);                                  // vertical zoom
+    
+}
+
+void LaGranaAudioProcessorEditor::loadBtnClicked() {
+
+    juce::FileChooser chooser("Select a Wave file to play...",
+        {},
+        "*.wav");                                        // [7]
+
+    if (chooser.browseForFileToOpen())                                          // [8]
+    {
+        auto file = chooser.getResult();                                        // [9]
+        auto* reader = formatManager.createReaderFor(file);                    // [10]
+
+        if (reader != nullptr)
+        {
+            std::unique_ptr<juce::AudioFormatReaderSource> newSource(new juce::AudioFormatReaderSource(reader, true)); // [11]
+            transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);                                 // [12]                                                                           // [13]
+            thumbnail->setSource(new juce::FileInputSource(file));
+            readerSource.reset(newSource.release());                                                                    // [14]
+
+            
+        }
+    }
+}
+
+void LaGranaAudioProcessorEditor::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+    if (source == &transportSource) transportSourceChanged();
+    if (source == thumbnail)       thumbnailChanged();
+}
+
+void LaGranaAudioProcessorEditor::transportSourceChanged()
+{
+    //changeState(transportSource.isPlaying() ? Playing : Stopped);
+}
+
+void LaGranaAudioProcessorEditor::thumbnailChanged()
+{
+    repaint();
 }
--- Grana/Source/PluginEditor.h
+++ Grana/Source/PluginEditor.h
@@ -14,7 +14,7 @@
 //==============================================================================
 /**
 */
-class LaGranaAudioProcessorEditor  : public juce::AudioProcessorEditor
+class LaGranaAudioProcessorEditor  : public juce::AudioProcessorEditor, public juce::ChangeListener
 {
 public:
     LaGranaAudioProcessorEditor (LaGranaAudioProcessor&);
@@ -24,10 +24,33 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     LaGranaAudioProcessor& audioProcessor;
+
+    juce::TextButton *loadBtn;
+    juce::AudioFormatManager formatManager;
+    juce::AudioTransportSource transportSource;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+
+    juce::AudioThumbnailCache* thumbnailCache;                  // [1]
+    juce::AudioThumbnail* thumbnail;                            // [2]
+
+
+    void paintIfNoFileLoaded(juce::Graphics& g, const juce::Rectangle<int>& thumbnailBounds);
+
+    void paintIfFileLoaded(juce::Graphics& g, const juce::Rectangle<int>& thumbnailBounds);
+
+    void loadBtnClicked();
+
+
+
+    void transportSourceChanged();
+
+    void thumbnailChanged();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LaGranaAudioProcessorEditor)
 };
