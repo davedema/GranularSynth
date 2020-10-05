79ea2f4cd3621044d8fe456e5c350ab270084c1a
Waddafunk <jacopo.piccirillo@gmail.com>
2020-10-05 19:17:37 +0200

    rand in [-1, 1]

--- Grana/Source/SequenceStrategy.cpp
+++ Grana/Source/SequenceStrategy.cpp
@@ -18,6 +18,6 @@ SequenceStrategy::SequenceStrategy()
 
 int SequenceStrategy::nextInterOnset()
 {
-    float randomNumber = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    float randomNumber = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5f) * 2.0f; //rand in [-1, 1]
     return (1 / grainDensity) + randomNumber * quasiSyncRange;
 }
