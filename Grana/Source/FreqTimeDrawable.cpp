4a144ef7281a58956f8bf4f738b9314f3d76509d
ammlyy <amerenamatteo@gmail.com>
2020-12-08 14:32:04 +0100

    fixed xy plane

--- Grana/Source/FreqTimeDrawable.cpp
+++ Grana/Source/FreqTimeDrawable.cpp
@@ -42,20 +42,6 @@ void FreqTimeDrawable::paint(Graphics&g)
         g.setColour(Colour(ColourPalette::numbers));
         g.strokePath(path, PathStrokeType(3.5f));
     }
-    g.setFont(10);
-
-    g.setColour(Colour(ColourPalette::bright_component));
-    g.drawText("Freq. shift [Hz]", getWidth() / 2 - 3, 5, 100, 6, Justification::centred, false);
-    g.drawText("Time [ms]", getWidth() - 85, getHeight() / 2 + 8, 100, 6, juce::Justification::centred, false);
-
-    g.drawLine(getWidth() / 2 - 3, 5, getWidth() / 2 + 3, 5); //2k tick
-    g.drawText("+2k", getWidth() / 2 - 25, 5, 18, 5, juce::Justification::centred, false);
-    
-    g.drawLine(getWidth() / 2 - 3, getHeight() - 5, getWidth() / 2 + 3, getHeight() - 5); // -2k tick
-    g.drawText("-2k", getWidth() / 2 - 25, getHeight() - 10, 18, 5, juce::Justification::centred, false);
-
-
-    g.drawHorizontalLine(5, 3, 3);
 
     enlightPoint(model->getCurrentxyPosition(), g);
 
@@ -88,15 +74,15 @@ void FreqTimeDrawable::setModel(Model* model)
 
 void FreqTimeDrawable::enlightPoint(Point<float> point, Graphics& g)
 {
-    g.setColour(Colours::white);
+    g.setColour(Colour(ColourPalette::bright_component));
     g.drawEllipse(point.getX() * getWidth(), point.getY() * getHeight(), 4, 4, 2);
 }
 
 void FreqTimeDrawable::drawAxis(Graphics&g)
 {
     auto margin = 5.0f;
-    Line<float> freq_ax(Point<float>(getWidth() / 2, (float)getHeight() - margin),
-        Point<float>((float)getWidth() / 2, margin));
+    Line<float> freq_ax(Point<float>(margin, (float)getHeight() - margin),
+        Point<float>((float)margin, margin));
 
     Line<float> time_ax(Point<float>(margin, (float)getHeight() / 2 ),
         Point<float>((float)getWidth() - margin, (float)getHeight()/2));
@@ -104,6 +90,18 @@ void FreqTimeDrawable::drawAxis(Graphics&g)
 
     g.drawLine(freq_ax);
     g.drawLine(time_ax);
+
+    g.setFont(10);
+
+    g.drawText("Freq. shift [Hz]", margin + 10, 5, 100, 6, Justification::centred, false);
+    g.drawText("Time", getWidth() - 70, getHeight() / 2 + 8, 100, 6, juce::Justification::centred, false);
+
+    g.drawLine(margin - 3, 5, margin + 3, 5); //2k tick
+    g.drawText("+2k", margin + 1, 5, 18, 5, juce::Justification::centred, false);
+
+    g.drawLine(margin - 3, getHeight() - margin, margin + 3, getHeight() - margin); // -2k tick
+    g.drawText("-2k", margin + 1, getHeight() - 10, 18, 5, juce::Justification::centred, false);
+
 
 }
 
